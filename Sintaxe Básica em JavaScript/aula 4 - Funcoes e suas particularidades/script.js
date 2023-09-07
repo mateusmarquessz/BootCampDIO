dc32d02fcc8907dea34cbd14a3623555e6b2504d
MateusMarquessz <mateusmarques53@outlook.com>
2023-09-07 13:34:06 -0300

    att

---
+++ Sintaxe Básica em JavaScript/aula 4 - Funcoes e suas particularidades/script.js
@@ -0,0 +1,15 @@
+function funcao(){
+    console.log("Tudo certo jovem!");
+}
+
+funcao();
+
+
+function mensagem(primeiro, segundo)
+{
+    console.log(primeiro, segundo);
+}
+
+
+
+mensagem('tudo certo', 'jovem');
---
+++ Sintaxe Básica em JavaScript/aula 5 - aprofudando em funcoes/script.js
@@ -0,0 +1,21 @@
+//Tipos de funcao
+
+//declarativas
+function funcao(){
+    console.log('tudo certo jovem');
+}
+
+funcao();
+
+//Expressoes de funcoes
+var x = function(){
+    console.log('tudo certo jovem');
+}
+
+x();
+
+//arroy function
+
+var y = () =>{
+    console.log('tudo certo jovem');
+}
