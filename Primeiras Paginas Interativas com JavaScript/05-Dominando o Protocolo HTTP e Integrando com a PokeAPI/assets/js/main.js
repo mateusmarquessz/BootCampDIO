6c7d5176516090d4eb64a88ddb96cf92235522b3
MateusMarquessz <mateusmarques53@outlook.com>
2023-09-18 10:10:59 -0300

    att

--- Primeiras Paginas Interativas com JavaScript/05-Dominando o Protocolo HTTP e Integrando com a PokeAPI/assets/js/main.js
+++ Primeiras Paginas Interativas com JavaScript/05-Dominando o Protocolo HTTP e Integrando com a PokeAPI/assets/js/main.js
@@ -1,8 +1,10 @@
-const offset=0
+const offset= 0
 const limit = 10
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
 
-fetch(url).then(function(response){
-    console.log(response)
-})
+//manipula o sucesso
+fetch(url)
+    .then((response) => response.json()) //retorno da url
+    .then((jsonBody) => console.log(jsonBody)) //retorno do primeiro then
+    .catch((error) =>console.error(error))
